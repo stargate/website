5e3f62069320e659faee385336667a0354a85063
Drew Witmer <drew.witmer@gmail.com>
2020-09-04 16:59:11 -0400

    Enabled posts, travis and updated settings

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source "https://rubygems.org"
 
-gem "jekyll", "~> 4.1.1"
+# gem "jekyll", "~> 4.1.1"
+gem "jekyll", "~> 3.9.0"
+
+gem 'github-pages', group: :jekyll_plugins
 
 group :jekyll_plugins do
   gem "jekyll-feed"
--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,14 @@
 const jekyllEnv = process.env.JEKYLL_ENV || "development";
 
 module.exports = {
+  parser: 'postcss-scss',
   plugins: [
     require("autoprefixer"),
     ...(jekyllEnv != "development"
       ? [
-        require("@fullhuman/postcss-purgecss")({
-          content: ["!(_site|node_modules)/**/*.+(html|js|md)", "*.html"],
-          defaultExtractor: (content) =>
-            content.match(/[\w-/:]+(?<!:)/g) || [],
-        }),
+        // require("@fullhuman/postcss-purgecss")({
+        //   content: ["!(_site|node_modules)/**/*.+(html|js|md)", "*.html"]
+        // }),
         require("cssnano")({ preset: "default" }),
       ]
       : [])
